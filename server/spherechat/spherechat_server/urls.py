768076f972894659e4d8dd5b75a33d92bfd8317a
Alexandre Barone <xblade45@gmail.com>
2017-03-18 14:33:11 -0400

    changed url paths

--- server/spherechat/spherechat_server/urls.py
+++ server/spherechat/spherechat_server/urls.py
@@ -25,6 +25,6 @@ router.register(r'groups', views.GroupViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^server/', include(router.urls)),
-    url(r'^server/api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
