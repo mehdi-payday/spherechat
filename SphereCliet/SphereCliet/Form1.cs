418e2a9578b548db2982fea43666c928e7eca46d
ninespin <ninespinftw@gmail.com>
2017-03-20 15:32:49 -0400

    prototype 1 for desktop client

---
+++ SphereCliet/SphereCliet/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SphereCliet {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void pictureBox4_Click( object sender, EventArgs e ) {
+
+        }
+
+        private void panel11_Paint( object sender, PaintEventArgs e ) {
+
+        }
+
+        private void label6_Click( object sender, EventArgs e ) {
+
+        }
+
+        private void panel4_Paint( object sender, PaintEventArgs e ) {
+
+        }
+
+        private void label24_Click( object sender, EventArgs e ) {
+
+        }
+
+        private void pictureBox13_Click( object sender, EventArgs e ) {
+
+        }
+
+        private void label23_Click( object sender, EventArgs e ) {
+
+        }
+
+        private void tabPage1_Click( object sender, EventArgs e ) {
+
+        }
+
+        private void label29_Click( object sender, EventArgs e ) {
+
+        }
+
+        private void label30_Click( object sender, EventArgs e ) {
+
+        }
+
+        private void pictureBox14_Click( object sender, EventArgs e ) {
+
+        }
+
+        private void label26_Click( object sender, EventArgs e ) {
+
+        }
+    }
+}
