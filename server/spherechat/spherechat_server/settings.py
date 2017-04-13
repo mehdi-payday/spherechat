c8d9238d940e72232da8788caa7e814c018a5b60
Adam Cherti <adam@cherti.name>
2017-04-12 23:20:03 -0400

    fix permissions bug

--- server/spherechat/spherechat_server/settings.py
+++ server/spherechat/spherechat_server/settings.py
@@ -64,7 +64,7 @@ SITE_ID = 1
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAdminUser'
+        'rest_framework.permissions.IsAuthenticated'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
