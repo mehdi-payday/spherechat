60dc711c07bffb2d8c73cd8d1255c25dea1b5b27
Alexandre Barone <xblade45@gmail.com>
2017-04-15 18:15:16 -0400

    added animations between views, but there is still flickering bug caused by custom css per page

--- web_client/app/app.js
+++ web_client/app/app.js
@@ -14,6 +14,7 @@ angular
 		'myApp.main',
 		'myApp.webclient',
 		'ngRoute',
+		'ngAnimate',
 		'routeStyles',
 		'api',
 		'auth',
@@ -151,35 +152,6 @@ angular
 			          ]
 			})
 			.otherwise({redirectTo: '/'});
-	}])
-
-	.directive('head', ['$rootScope','$compile', function($rootScope, $compile) {
-		return {
-		    restrict: 'E',
-		    link: function(scope, elem){
-		        var html = '<link rel="stylesheet" ng-repeat="(routeCtrl, cssUrl) in routeStyles" ng-href="{{cssUrl}}" />';
-		        elem.append($compile(html)(scope));
-		        scope.routeStyles = {};
-		        $rootScope.$on('$routeChangeStart', function (e, next, current) {
-		            if(current && current.$$route && current.$$route.css){
-		                if(!angular.isArray(current.$$route.css)){
-		                    current.$$route.css = [current.$$route.css];
-		                }
-		                angular.forEach(current.$$route.css, function(sheet){
-		                    delete scope.routeStyles[sheet];
-		                });
-		            }
-		            if(next && next.$$route && next.$$route.css){
-		                if(!angular.isArray(next.$$route.css)){
-		                    next.$$route.css = [next.$$route.css];
-		                }
-		                angular.forEach(next.$$route.css, function(sheet){
-		                    scope.routeStyles[sheet] = sheet;
-		                });
-		            }
-		        });
-		    }
-		};
 	}])
 	
     .run(function($rootScope, $location, session, auth) {
