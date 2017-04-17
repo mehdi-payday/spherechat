09485c6e907080849cc46d916e6baf7369400181
Alexandre Barone <xblade45@gmail.com>
2017-04-17 11:35:57 -0400

    api service tests completed

--- web_client/app/tests/api.spec.js
+++ web_client/app/tests/api.spec.js
@@ -1,6 +1,6 @@
 
 describe('api factory tests', function() {
-	
+	var serverAddress;
 	var api;
 	var $httpBackend;
 	
@@ -14,6 +14,7 @@ describe('api factory tests', function() {
 	
 	beforeEach(inject(function(_api_){
 		api = _api_;
+		serverAddress = api.serverAddress;
 	}));
 	
 	it('$httpBackend object should exist', function(){
@@ -22,5 +23,21 @@ describe('api factory tests', function() {
 	
 	it('api factory object should exist', function(){
 		expect(api).toBeDefined();
+	});
+	
+	it('api serverAddress should exist', function(){
+		expect(serverAddress).toBeDefined();
+	});
+	
+	it('api login function', function(){
+		$httpBackend.expect('POST', serverAddress + 'api/auth/login/', {username: 'test', password: 'test'}).respond(200, {key: 'x'});
+		api.login.save({}, {username: 'test', password: 'test'});
+		$httpBackend.flush();
+	});
+	
+	it('api register function', function(){
+		$httpBackend.expect('POST', serverAddress + 'api/auth/registration/', {username: 'test', email: 'test', password1: 'test', password2: 'test'}).respond(200, {key: 'x'});
+		api.register.save({}, {username: 'test', email: 'test', password1: 'test', password2: 'test'});
+		$httpBackend.flush();
 	});
 });
