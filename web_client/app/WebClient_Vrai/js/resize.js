92a2ed8b9aa38c60f7733445f29473f29b6eaaf1
xSash <big.sash11@gmail.com>
2017-04-17 14:05:36 -0400

    menu.js

--- web_client/app/WebClient_Vrai/js/resize.js
+++
@@ -1,8 +0,0 @@
-      function textAreaAdjust(o) {
-        var inp = $("#comment");
-        o.style.height = "1px";
-        o.style.height = (o.scrollHeight)+"px";
-        if(inp.val().lenght <= 0){
-          o.height = "55px";
-        }
-      }
