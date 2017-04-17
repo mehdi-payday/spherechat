dbdd005dc36fe3f3d4c959b352318ee4c412cf61
Alexandre Barone <xblade45@gmail.com>
2017-04-17 13:19:43 -0400

    added more tests for messaging service

--- web_client/app/tests/messaging.spec.js
+++ web_client/app/tests/messaging.spec.js
@@ -1,6 +1,7 @@
 
 describe('messaging Service tests', function(){
 	
+	var session;
 	var messaging;
 	var serverAddress;
 	var $httpBackend;
@@ -13,9 +14,10 @@ describe('messaging Service tests', function(){
 		$httpBackend = _$httpBackend_;
 	}));
 	
-	beforeEach(inject(function(_messaging_, _api_){
+	beforeEach(inject(function(_messaging_, _api_, _session_){
 		messaging = _messaging_;
 		serverAddress = _api_.serverAddress;
+		session = _session_;
 	}));
 	
 	it('$httpBackend object should exist', function(){
@@ -24,5 +26,46 @@ describe('messaging Service tests', function(){
 	
 	it('messaging object should exist', function(){
 		expect(messaging).toBeDefined();
+	});
+	
+	it('session object should exist', function(){
+		expect(session).toBeDefined();
+	});
+	
+	it('createChannel should post Authorization header', function(){
+		session.setAuthToken('x');
+		$httpBackend.expect('POST', serverAddress + 'api/messaging/channel/', undefined, function(headers){
+			return headers['Authorization'] === 'Token x';
+		}).respond(200, 'success');
+		messaging.createChannel();
+		$httpBackend.flush();
+	});
+	
+	it('getChannel should return Channel Object', function(){
+		session.setAuthToken('x');
+		$httpBackend.expect('GET', serverAddress + 'api/messaging/channel/1/').respond(200, 'success');
+		messaging.getChannel(1);
+		$httpBackend.flush();
+	});
+	
+	it('getChannels should return array of Channel Objects', function(){
+		session.setAuthToken('x');
+		$httpBackend.expect('GET', serverAddress + 'api/messaging/channel/').respond(200, ['success']);
+		messaging.getChannels();
+		$httpBackend.flush();
+	});
+	
+	it('getChannelMessages should return array of Message Objects per channel id', function(){
+		session.setAuthToken('x');
+		$httpBackend.expect('GET', serverAddress + 'api/messaging/channel/1/message/').respond(200, ['success']);
+		messaging.getChannelMessages(1);
+		$httpBackend.flush();
+	});
+	
+	it('sendChannelMessage should return Message Object', function(){
+		session.setAuthToken('x');
+		$httpBackend.expect('POST', serverAddress + 'api/messaging/channel/1/message/').respond(200, 'success');
+		messaging.sendChannelMessage(1, "Hello");
+		$httpBackend.flush();
 	});
 });
