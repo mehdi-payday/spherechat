25fb0d2b0c914cbf273ecc7a1e2d4d0db2b7fe4a
adimux <adam@cherti.name>
2017-10-02 13:38:37 -0400

    Update urls.py

--- server/spherechat/spherechat_server/urls.py
+++ server/spherechat/spherechat_server/urls.py
@@ -1,17 +1,5 @@
-"""spherechat_server URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+spherechat_server URL Configuration
 """
 
 from django.conf.urls import url, include
@@ -27,11 +15,8 @@ router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet, base_name='users')
 router.register(r'groups', views.GroupViewSet, base_name='groups')
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^api/', include(router.urls)),
-#    url(r'^admin/', include('admin.site.urls')),
     url(r'^api/django-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/messaging/', include('messaging.urls')),
     url(r'^api/friendship/', include('friendship.urls')),
@@ -39,4 +24,4 @@ urlpatterns = [
     url(r'^api/me/change_password/', views.ChangePasswordView.as_view()),
     url(r'^api/auth/', include('rest_auth.urls')),
     url(r'^api/auth/registration/', include('rest_auth.registration.urls'))
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
