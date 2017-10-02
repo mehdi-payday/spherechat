285b6c07528ea1974eec27e618a31071a6e3d425
adimux <adam@cherti.name>
2017-10-02 13:20:16 -0400

    update connection.py with descriptive comments

--- server/spherechat/core/connection.py
+++ server/spherechat/core/connection.py
@@ -11,7 +11,21 @@ def connection_factory(auth_class, pubsub):
             super(BasicConnection, self).on_message(msg)
 
         """
-        @return {boolean} publish : publish the message
+        Calls the appropriate channel handler following the channel name.
+        If no handler is found, the function returns True, which just broadcasts the message to the 
+        omnibus channel.
+        
+        If the right format of channel is used. The function imports {handler} from {module}.channels and
+        calls the handler function passing to it {subchannel} as the channel name.
+        
+        Parameters :
+        ------------
+        channel - str
+            The channel "route".
+            {module},{handler}.{subchannel}
+        Return 
+        ------
+        boolean - True to publish right away the message to the omnibus channel; False to do nothing.
         """
         def call_handler(self, channel, payload, **kwargs):
             module = ''
@@ -21,46 +35,24 @@ def connection_factory(auth_class, pubsub):
             comma_index = channel.index(",")
             first = channel[0:comma_index]
             last = channel[comma_index+1:]
-
-            print "First"
-            print first
-            print "Last"
-            print last
-
-#       parts = first.split(".")
+            
             module = first       
-#       if len(parts) < 1:
-#           return True
-#       else:
-#           schema_name = parts[0]
-#           module = ".".join(parts[1:])
 
             parts = last.split(".")
-
-            print "Parts"
-            print parts
 
             if len(parts) is not 2:
                 return True
             else:
                 handler = parts[0]
                 subchannel = parts[1]
-
-            print "Parts"
-            print parts
-
-#            connection.schema_name = schema_name
+                
             twodots_index = payload.index(":")
 
             data = simplejson.loads(payload[twodots_index+1:])
             mod = import_module(module + ".channels")
 
             handler_class = getattr(mod, handler)
-
-#       handler_class = getattr(import_module(module), handler)
-            print "Handler"
-            print handler_class
-        
+            
             handler_instance = handler_class(self)
 
             action = handler_instance.handle_channel_message(subchannel, data)
@@ -68,18 +60,12 @@ def connection_factory(auth_class, pubsub):
             return action
 
         def on_channel_message(self, channel, payload):
-            print "On channel message on %s" % channel
 
             if (channel in self.subscriber.channels
                     and self.authenticator.can_publish(channel)):
-                print "channel"
-                print channel
-
+                
                 publish_it = self.call_handler(channel, payload)
 
-                print "Republish it ??"
-                print publish_it
-    
                 if publish_it == True:
                     self.publish(payload)
 
